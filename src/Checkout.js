0d4e54100403ca56fea8e045829634e518dd6a23
Kebogile <kebogile1201@icloud.com>
2020-08-14 12:40:30 +0200

    merge local to remote

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useStateValue } from "./StateProvider";
 import "./checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {
   const [{ basket }] = useStateValue();
@@ -11,7 +12,7 @@ function Checkout() {
         src={require("./images/checkout.jpg")}
         alt="checkout"
       />
-
+      {/* if there is nothing in the basket */}
       {basket?.length === 0 ? (
         <div>
           <h2 className="checkout_title">Your shopping cart is empty</h2>
@@ -21,8 +22,19 @@ function Checkout() {
           </p>
         </div>
       ) : (
+        // otherwise if items are in the cart
         <div>
           <h2 className="checkout_title"> Review your Shopping cart</h2>
+          {/* list out all the products on the baskey  */}
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       )}
     </div>
